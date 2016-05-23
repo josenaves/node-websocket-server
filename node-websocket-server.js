299644a03e526f756daa3b48901c83d38df990e4
José Naves Moura Neto <josenaves@gmail.com>
2016-05-23 19:19:38 -0300

    Add response on connection

--- node-websocket-server.js
+++ node-websocket-server.js
@@ -5,47 +5,43 @@ var ProtoBuf = require('protobufjs');
 var builder = ProtoBuf.loadProtoFile("./image.proto");
 
 var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ server: '192.168.0.16', port: 9090 });
+var wss = new WebSocketServer({ server: '192.168.0.16', port: 9080 });
 
 var imagesServed = 0;
 
 wss.on('connection', function connection(ws) {
-  
   console.log('connected');
+  console.log('responding...');
+  
+  var buffer = getFlorianopolisPicture();
+  ws.send(buffer.toArrayBuffer(), { binary: true, mask: true });
 
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  imagesServed++;
+  console.log(imagesServed + ' images served');
 
-    imagesServed++;
-    console.log('responding...');
-    var buffer = buildRandomMessage();
-    ws.send(buffer.toArrayBuffer(), { binary: true, mask: true });
-	console.log(imagesServed + ' images served');
-  });
-
+  // ws.on('message', function incoming(message) {
+  //   console.log('received: %s', message);
+  //   imagesServed++;
+  //   console.log('responding...');
+  //   var buffer = buildRandomMessage();
+  //   ws.send(buffer.toArrayBuffer(), { binary: true, mask: true });
+  //   console.log(imagesServed + ' images served');
+  // });
 });
 
-function buildRandomMessage() {	
+function getFlorianopolisPicture() {
+  var Image = builder.build('com.josenaves.android.pb.restful.Image');
 
-  var Image = builder.build('com.josenaves.android.websocket.client.Image');
   var id = uuid.v1();
+  var name = 'florianopolis.jpg';
+  var data = fs.readFileSync(name);
   var datetime = new Date().toISOString()
-  	.replace(/T/, ' ')      // replace T with a space
-  	.replace(/\..+/, '');
-
-  var randomImg = randomImage();
-  var name = randomImg.name;
-  var image = new Image(id, name, datetime, randomImg.data);
+    .replace(/T/, ' ')      // replace T with a space
+    .replace(/\..+/, '');
   
-  return image.encode();
+  var imagePB = new Image(id, name, datetime, data);
+  
+  return imagePB.encode();
 }
 
-function randomImage() {
-  var images = ['florianopolis.jpg', 'io2016.jpg', 'google-io16.png'];
-  var idx = Math.floor(Math.random() * images.length);
-  console.log('Random image is ' + images[idx]);
-  return { name: images[idx], data: fs.readFileSync(images[idx]) };
-}
-
-console.log('Server up. Listening on port 9090...');
-//buildRandomMessage();
+console.log('Listening on port 9080...');
